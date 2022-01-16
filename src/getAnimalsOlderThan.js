c6a4c699ac16a0a49d2f5e1aaada1d34b0b5865d
kodenart <mjuanvitorp@gmail.com>
2022-01-16 01:51:35 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species
+    .find((specie) => specie.name === animal).residents
+    .every((resident) => resident.age > age);
 }
+
+// console.log(getAnimalsOlderThan('lions', 6));
 
 module.exports = getAnimalsOlderThan;

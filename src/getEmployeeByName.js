6be954c9ac40602e9c2b8f774eada5dde8cf4053
kodenart <mjuanvitorp@gmail.com>
2022-01-16 02:08:49 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const returnedEmployee = data.employees
+    .find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
+  // separei para ficar mais legível o que está acontecendo, ficou um pouco confuso quando na mesma linha
+  return returnedEmployee === undefined ? {} : returnedEmployee;
 }
+
+// console.log(getEmployeeByName('EdPe'));
 
 module.exports = getEmployeeByName;

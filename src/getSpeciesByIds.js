9c5181390595249e2e8f476b11b235c10d185c90
kodenart <mjuanvitorp@gmail.com>
2022-01-16 01:27:35 -0300

    finalmente iniciando o projeto =)

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return ids.map((inputId) => data.species.find((specie) => specie.id === inputId));
 }
+
+// console.log(getSpeciesByIds(data.species[0].id, data.species[1].id));
 
 module.exports = getSpeciesByIds;

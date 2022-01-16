cfef88d57830873c35db3032eb2c630b649e13b9
kodenart <mjuanvitorp@gmail.com>
2022-01-16 04:55:11 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,29 @@
 const data = require('../data/zoo_data');
 
+// função que vai retornar o objeto com todos animais...
+const allAnimals = () => {
+  const allAnimalsArr = {};
+  data.species.forEach((currSpec) => {
+    allAnimalsArr[currSpec.name] = currSpec.residents.length;
+  });
+  return allAnimalsArr;
+};
+
+const findBySpecies = (specie) => data.species.find((curValue) => curValue.name === specie);
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return allAnimals();
+  }
+  const { specie, sex } = animal;
+  if (sex === undefined) {
+    return findBySpecies(specie).residents.length;
+  }
+  return findBySpecies(specie).residents
+    .filter((resident) => resident.sex === sex)
+    .length;
 }
+
+// console.log(countAnimals({ specie: 'frogs', sex: 'female' }));
 
 module.exports = countAnimals;

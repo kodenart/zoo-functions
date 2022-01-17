290ead3d320fe5f9157d4ded3411431cfc15510d
kodenart <mjuanvitorp@gmail.com>
2022-01-17 01:59:55 -0300

    desistindo do requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const noOptionsGiven = (locArr) => {
+  const finalObj = {};
+  locArr.forEach((loc) => {
+    finalObj[loc] = data.species.filter((specie) => specie.location === loc)
+      .map((selectedSpecies) => selectedSpecies.name);
+  });
+  return finalObj;
+};
+
+// const namesIncluded = (locArr) => {
+//   const objectToReturn = {};
+//   locArr.forEach((loc) => {
+//     objectToReturn[loc] = data.species.filter((species) => species.location === loc)
+//       .map((selectedSpecies) => ({ listinha: selectedSpecies.residents.map((resident) => resident.name) }));
+//   });
+//   return objectToReturn;
+// };
+
+function getAnimalMap({ includeNames, sex, sorted } = {}) {
+  const locationsArr = ['NE', 'NW', 'SE', 'SW'];
+  if (!includeNames) {
+    return noOptionsGiven(locationsArr);
+  }
+//   if (!sex && !sorted) {
+//     return namesIncluded(locationsArr);
+//   }
 }
 
 module.exports = getAnimalMap;

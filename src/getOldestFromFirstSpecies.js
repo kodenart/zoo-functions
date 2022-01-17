463c693230168e9db979eb086d3983a70b47b0b6
kodenart <mjuanvitorp@gmail.com>
2022-01-17 02:22:43 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstSpeciesId = employees.filter((employee) => employee.id === id)[0]
+    .responsibleFor[0];
+  const residentsFromId = species.filter((specie) => specie.id === firstSpeciesId)[0].residents;
+  residentsFromId.sort((residentA, residentB) => residentB.age - residentA.age);
+  return [residentsFromId[0].name, residentsFromId[0].sex, residentsFromId[0].age];
 }
+
+console.log(getOldestFromFirstSpecies(employees[0].id));
 
 module.exports = getOldestFromFirstSpecies;
